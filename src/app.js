5c037d6e47dd1151970d929a19dba035a8dfa4fa
Administrator <Administrator@DESKTOP-49AFC58>
2021-10-20 16:38:38 +0300

    javascript 1.gün

---
+++ src/app.js
@@ -0,0 +1,18 @@
+let dolarDun=9.20
+let dolarBugun=9.10
+{
+    let dolarDun=9.10;
+}
+console.log(dolarDun)
+
+const euroDun=11
+//euroDun=11 bu hata 
+console.log(euroDun)
+let konutKredileri=["konut kredisi","emlak konut kredisi","kamu konut kredisi"]
+console.log(konutKredileri)
+console.log("<ul>")
+for(let i= 0; i<konutKredileri.length; i++){
+    console.log(konutKredileri[i])
+
+}
+console.log("<ul>")
